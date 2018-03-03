661708904c3e90b262824a182f45e213465acb3f
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-03 16:27:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,4 +10,12 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     "#{@number * @number}"
   end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @number.times do
+      "#{@phrase}"
+    end
+  end
 end
