bb121c166d6a3df02a963b032531e566f5ebc2d3
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-03 16:44:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get '/:operation/:number1/:number2' do
-    eval "params[:number1] params[:operation] params[:number2]"
+  get 'add/:number1/:number2' do
+    eval "params[:number1] + params[:number2]"
   end
 end
